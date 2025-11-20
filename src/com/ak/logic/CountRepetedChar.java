d8bf27c2d9e1452b27e331d1eb895e4814ba05bb
Akshaya Kumar Mahanty <akshayakumarmahanty@LAP831PER-Akshayas-MacBook.local>
2025-11-20 14:41:58 +0530

    count repeted character add

---
+++ src/com/ak/logic/CountRepetedChar.java
@@ -0,0 +1,19 @@
+package com.ak.logic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountRepetedChar {
+
+    public static void main(String[] args) {
+
+        String name = "Akshaya";
+
+        Map<Character,Integer> map = new HashMap<>();
+        for(Character c : name.toLowerCase().toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        System.out.println(map);
+    }
+}
